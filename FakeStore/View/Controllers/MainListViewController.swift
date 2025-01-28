6f7ab2488d7cb5537a98dd956573112b09e7d0ed
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-01-28 19:29:57 -0400

    initial commit

---
+++ FakeStore/View/Controllers/MainListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeStore
+//
+//  Created by Andrew Vale on 28/01/25.
+//
+
+import UIKit
+
+class MainListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+
+}
+

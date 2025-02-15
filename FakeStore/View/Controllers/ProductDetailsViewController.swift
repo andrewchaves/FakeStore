c3c75d840f903479923f31e687934ada109b6d5d
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-02-15 12:49:58 -0400

    Finish Product detail screen layout

--- FakeStore/View/Controllers/ProductDetailsViewController.swift
+++ FakeStore/View/Controllers/ProductDetailsViewController.swift
@@ -13,6 +13,12 @@ class ProductDetailsViewController: UIViewController {
     
     var productToDisplay: ProductForUI
     
+    var contentScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
     var productView: UIView = {
         let view = UIView()
         view.layer.shadowColor = UIColor.gray.cgColor
@@ -47,7 +53,25 @@ class ProductDetailsViewController: UIViewController {
         var text = UITextView()
         text.font = UIFont.systemFont(ofSize: 18.0)
         text.textColor = .lightGray
+        text.isEditable = false
+        text.isScrollEnabled = false
         return text
+    }()
+    
+    var addToCartButton: GeneralButton = {
+        var button = GeneralButton(title: "Add to Cart",
+                                   titleBold: true,
+                                   titleTextColor: .white,
+                                   backgroundColor: .green)
+        return button
+    }()
+    
+    var buyButton: GeneralButton = {
+        var button = GeneralButton(title: "Buy it",
+                                   titleBold: true,
+                                   titleTextColor: .white,
+                                   backgroundColor: .blue)
+        return button
     }()
     
     init (productToDisplay: ProductForUI) {
@@ -61,56 +85,83 @@ class ProductDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bluidScreen()
         productImageView.sd_setImage(with: URL(string: productToDisplay.image))
         productTitle.text = productToDisplay.title
         productPrice.text = productToDisplay.price
         productDescription.text = productToDisplay.description
+        bluidScreen()
     }
     
     //MARK: - Setup
+
     func bluidScreen() {
         let halfScreenWidth = self.view.frame.size.width/2
+        let thirdScreenWidth = self.view.frame.size.width/3
+        let productDescriptionHeight = productDescription.sizeThatFits(CGSize(width: self.view.frame.width - 32,
+                                                                              height: CGFloat.greatestFiniteMagnitude)).height
         
         self.title = "Product Details"
         self.view.backgroundColor = .white
         
+        contentScrollView.translatesAutoresizingMaskIntoConstraints = false
         productView.translatesAutoresizingMaskIntoConstraints = false
         productImageView.translatesAutoresizingMaskIntoConstraints = false
         productTitle.translatesAutoresizingMaskIntoConstraints = false
         productPrice.translatesAutoresizingMaskIntoConstraints = false
         productDescription.translatesAutoresizingMaskIntoConstraints = false
+        addToCartButton.translatesAutoresizingMaskIntoConstraints = false
+        buyButton.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(productView)
+        self.view.addSubview(contentScrollView)
+        contentScrollView.addSubview(productView)
         productView.addSubview(productImageView)
-        self.view.addSubview(productTitle)
-        self.view.addSubview(productPrice)
-        self.view.addSubview(productDescription)
+        contentScrollView.addSubview(productTitle)
+        contentScrollView.addSubview(productPrice)
+        contentScrollView.addSubview(productDescription)
+        self.view.addSubview(addToCartButton)
+        self.view.addSubview(buyButton)
         
         NSLayoutConstraint.activate(
-            [productView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor,constant: 24),
-             productView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-             productView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            [contentScrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor,constant: 24),
+             contentScrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+             contentScrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+             contentScrollView.bottomAnchor.constraint(equalTo: addToCartButton.topAnchor, constant: -16),
+             
+             productView.topAnchor.constraint(equalTo: contentScrollView.topAnchor),
+             productView.leadingAnchor.constraint(equalTo: contentScrollView.leadingAnchor),
+             productView.trailingAnchor.constraint(equalTo: contentScrollView.trailingAnchor),
              productView.heightAnchor.constraint(equalToConstant: 320),
              
              productImageView.topAnchor.constraint(equalTo: productView.topAnchor),
              productImageView.leadingAnchor.constraint(equalTo: productView.leadingAnchor),
              productImageView.trailingAnchor.constraint(equalTo: productView.trailingAnchor),
              productImageView.bottomAnchor.constraint(equalTo: productView.bottomAnchor),
+             productImageView.widthAnchor.constraint(equalTo: contentScrollView.widthAnchor),
              
              productTitle.topAnchor.constraint(equalTo: productImageView.bottomAnchor,constant: 8),
-             productTitle.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+             productTitle.leadingAnchor.constraint(equalTo: contentScrollView.leadingAnchor, constant: 16),
              productTitle.widthAnchor.constraint(equalToConstant: halfScreenWidth),
              productTitle.heightAnchor.constraint(equalToConstant: 120),
              
              productPrice.topAnchor.constraint(equalTo: productImageView.bottomAnchor,constant: 8),
-             productPrice.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+             productPrice.trailingAnchor.constraint(equalTo: contentScrollView.trailingAnchor, constant: -16),
              productPrice.widthAnchor.constraint(equalToConstant: halfScreenWidth),
              
              productDescription.topAnchor.constraint(equalTo: productTitle.bottomAnchor,constant: 8),
-             productDescription.leadingAnchor.constraint(equalTo: productView.leadingAnchor),
-             productDescription.trailingAnchor.constraint(equalTo: productView.trailingAnchor),
-             productDescription.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+             productDescription.leadingAnchor.constraint(equalTo: contentScrollView.leadingAnchor),
+             productDescription.trailingAnchor.constraint(equalTo: contentScrollView.trailingAnchor),
+             productDescription.bottomAnchor.constraint(equalTo: contentScrollView.bottomAnchor),
+             productDescription.heightAnchor.constraint(equalToConstant: productDescriptionHeight),
+             
+             addToCartButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor,constant: -24),
+             addToCartButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+             addToCartButton.widthAnchor.constraint(equalToConstant: thirdScreenWidth),
+             addToCartButton.heightAnchor.constraint(equalToConstant: 60),
+             
+             buyButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor,constant: -24),
+             buyButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+             buyButton.widthAnchor.constraint(equalToConstant: halfScreenWidth),
+             buyButton.heightAnchor.constraint(equalToConstant: 60)
             ])
     }
 }

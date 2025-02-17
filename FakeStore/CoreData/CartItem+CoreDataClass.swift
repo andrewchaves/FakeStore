f4d45176a39d790627ed197fa51740330e4e83ed
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-02-17 16:35:00 -0400

    create a core data entity to save cart items

---
+++ FakeStore/CoreData/CartItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CartItem+CoreDataClass.swift
+//  FakeStore
+//
+//  Created by Andrew Vale on 17/02/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CartItem)
+public class CartItem: NSManagedObject {
+
+}
